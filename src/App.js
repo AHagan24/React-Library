14989652585910fbe7c1b0120804625d7ac23d32
AHagan24 <addisonhagan92@gmail.com>
2026-02-16 16:10:12 -0500

    v2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import Highlights from "./components/Highlights";
-import Landing from "./components/Landing";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={
+              <BookInfo books={books} addToCart={addToCart} cart={cart} />
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart books={books} cart={cart} changeQuantity={changeQuantity} />
+            }
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
